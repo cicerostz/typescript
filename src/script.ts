dd9ef7104632d6c5ce1730770966825ee1e4c878
Cicero <cicerodasilvatj@gmail.com>
2025-09-27 00:13:30 -0300

    AULA - Tipo guard

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,4 @@
 // Example: import from a valid relative path
-import { bootstrap } from "./modules/basic-types/intersection-types.js";
+import { bootstrap } from "./modules/narrowing/type-guard.js";
 
 export { bootstrap };
